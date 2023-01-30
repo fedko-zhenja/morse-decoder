f5be46c252cb3d8019de5e349e724890dbdcd7dc
Jhenja Fedko <fedkozhenja@gmail.com>
2023-01-30 05:13:45 +0400

    feat: implement function <morse-decoder>

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const codesArray = [];
+
+    for (let i = 0; i < expr.length; i = i + 10) {
+        codesArray.push(expr.slice(i, i + 10));
+    }
+
+    console.log(codesArray);
+
+    const arrayDotsDashes = codesArray.map((element) => {
+        if (element === '**********') {
+            return ' ';
+        }
+
+        let codeString = '';
+
+        for (let i = 0; i < element.length; i = i + 2) {
+            if (element[i] === '1' & element[i + 1] === '0') {
+                codeString += '.';
+            } else if (element[i] === '1' & element[i + 1] === '1') {
+                codeString += '-';
+            } else if (element[i] === '*') {
+                codeString += element[i] + element [i + 1];
+            }
+        }
+
+        return codeString;
+    } )
+
+    const decodedArray = arrayDotsDashes.map((element) => {
+        if (element === ' ') {
+            return element;
+        }
+        
+        for (key in MORSE_TABLE) {
+            if (element === key) {
+                return MORSE_TABLE[key];
+            }
+        }
+        
+    })
+
+    return decodedArray.join('');
 }
 
 module.exports = {
